4f3564978bc0314fb5708ecc7b7af5453fd4fad7
Guo Yanpei <73223812+GYPpro@users.noreply.github.com>
2025-02-05 00:01:34 +0800

    添加对MACOS的支持

--- include/platform.hpp
+++ include/platform.hpp
@@ -11,4 +11,9 @@ const string commandRedirect = "";
 const string historyTic = "=";
 const string REGEX = "=([0-9]*)ms";
 const string commandPrefix = "ping -n 1 -w ";
+#elif __APPLE__
+const string commandRedirect = "2>/dev/null";
+const string historyTic = "■";
+const string REGEX = "time=([0-9]+\\.?[0-9]*) ms";
+const string commandPrefix = "ping -c 1 -W ";
 #endif
